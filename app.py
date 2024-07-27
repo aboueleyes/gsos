09ac56dd5d9b7f0c7ac33159c758d5f735608620
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2024-07-27 15:51:43 +0300

    fix bug

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/') 
 def index():
-    return "<h1>Hello, World</h1>"
+    return "<h1>Hello, World!</h1>"
 
 
 if __name__ == "__main__":
